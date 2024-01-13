0700c7048a939864616c7ad7b21372e842ae3a91
DDbrother <DDbrother@gmail.com>
2024-01-13 12:14:43 -0500

    implement ACF

---
+++ src/main/java/net/Star/Commands/TestCommand.java
@@ -0,0 +1,7 @@
+package net.Star.Commands;
+
+import co.aikar.commands.BaseCommand;
+
+public class TestCommand extends BaseCommand{
+
+}
--- src/main/java/net/Star/Star.java
+++ src/main/java/net/Star/Star.java
@@ -1,5 +1,7 @@
 package net.Star;
 
+import co.aikar.commands.PaperCommandManager;
+
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -7,16 +9,24 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Star extends JavaPlugin implements Listener {
+import net.Star.Commands.TestCommand;
 
+public class Star extends JavaPlugin implements Listener {
+    private PaperCommandManager commandManager;
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        commandManager = new PaperCommandManager(this);
+
+        registerCommands();
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+    private void registerCommands() {
+        commandManager.registerCommand(new TestCommand());
     }
 
 }
