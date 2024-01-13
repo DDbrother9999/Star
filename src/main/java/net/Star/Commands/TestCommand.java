bd1bef77d0a2bad7225fa51482fe0691b2db0129
DDbrother <DDbrother@gmail.com>
2024-01-13 12:25:57 -0500

    implement TestCommand

--- src/main/java/net/Star/Commands/TestCommand.java
+++ src/main/java/net/Star/Commands/TestCommand.java
@@ -1,7 +1,17 @@
 package net.Star.Commands;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.Subcommand;
+import net.kyori.adventure.text.Component;
+import org.bukkit.entity.Player;
 
+@CommandAlias("test")
 public class TestCommand extends BaseCommand{
+
+    @Subcommand("help")
+    public void onTestHelp(Player player){
+        player.sendMessage(Component.text("Hello, " + player + "!" + "You've done the command /test help"));
+    }
 
 }
